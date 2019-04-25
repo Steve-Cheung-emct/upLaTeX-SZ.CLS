729e23a11448a3693322c453bfa1ececc6369ba2
Steve-Cheung-emct <dongfang0571@outlook.com>
2019-04-26 03:18:12 +0800

    Add files via upload

---
+++ 編譯 my sample.bat
@@ -0,0 +1,20 @@
+chcp 65001
+uplatex mysample
+ptex2pdf -l -u -ot "-kanji=utf8 " -od "-p B5 "   mysample
+#pause
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lol /s
+del *.lot /s
+del *.ent /s
+del *.fdb_latexmk /s
+del *.synctex.gz /s
+exit
