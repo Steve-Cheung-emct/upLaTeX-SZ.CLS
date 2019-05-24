15ae459c340e543209a4051bdcc0d7d80102c6c7
Steve-Cheung-emct <dongfang0571@outlook.com>
2019-05-24 22:59:08 +0800

    Add files via upload

--- 刪除.bat
+++ 刪除.bat
@@ -1,16 +1,1 @@
-del *.aux /s
-del *.bak /s
-del *.log /s
-del *.bbl /s
-del *.dvi /s
-del *.blg /s
-del *.thm /s
-del *.toc /s
-del *.out /s
-del *.lof /s
-del *.lol /s
-del *.lot /s
-del *.ent /s
-del *.fdb_latexmk /s
-del *.synctex.gz /s
-exit
+del	*.aux	*.bak	*.log	*.bbl	*.blg	*.thm	*.toc	*.out	*.lof	*.lol	*.lot	*.fdb_latexmk	*.synctex.gz	*.ent	/s
