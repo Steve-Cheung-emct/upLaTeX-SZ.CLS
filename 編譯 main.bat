a04e36415357813035d3b63b0ff9b3d3f155ef7a
Steve-Cheung-emct <dongfang0571@outlook.com>
2019-05-24 22:58:39 +0800

    Add files via upload

--- 編譯 main.bat
+++ 編譯 main.bat
@@ -1,20 +1,6 @@
 chcp 65001
+latexmk -c
 uplatex main
-ptex2pdf -l -u -ot "-kanji=utf8 " -od "-p B5 " main
-#pause
-del *.aux /s
-del *.bak /s
-del *.log /s
-del *.bbl /s
-del *.dvi /s
-del *.blg /s
-del *.thm /s
-del *.toc /s
-del *.out /s
-del *.lof /s
-del *.lol /s
-del *.lot /s
-del *.ent /s
-del *.fdb_latexmk /s
-del *.synctex.gz /s
+ptex2pdf -l -u -ot "-kanji=utf8 "  -od "-p B5 " main
+del	*.aux	*.bak	*.log	*.bbl	*.blg	*.thm	*.toc	*.out	*.lof	*.lol	*.lot	*.fdb_latexmk	*.synctex.gz	*.ent	/s
 exit
