40c6112cbd0fa740d0d1a4489620a32b5a809192
Steve-Cheung-emct <dongfang0571@outlook.com>
2019-09-29 15:08:00 +0800

    糾正bug

--- 編譯 main.bat
+++ 編譯 main.bat
@@ -1,6 +1,7 @@
 chcp 65001
 latexmk -c
 uplatex main
-ptex2pdf -l -u -ot "-kanji=utf8 "  -od "-p B5 " main
+uplatex main
+dvipdfmx main
 del	*.aux	*.bak	*.log	*.bbl	*.blg	*.thm	*.toc	*.out	*.lof	*.lol	*.lot	*.fdb_latexmk	*.synctex.gz	*.ent	/s
 exit
